3d1e9a2eaf4531b87277eb4c9f8af44b5d96ab34
Shelby Sturgis <shelbyjsturgis@gmail.com>
2016-01-03 11:56:18 -0800

    moving chart components into the index.js file of the chart directory and out of the directive

--- public/chart/index.js
+++ public/chart/index.js
@@ -1,10 +1,15 @@
 var d3 = require('d3');
 var _ = require('lodash');
+var baseLayout = require('plugins/tagcloud/chart/components/layout/generator');
 var builder = require('plugins/tagcloud/chart/components/utils/builder');
+var control = require('plugins/tagcloud/chart/components/control/events');
 var tagCloud = require('plugins/tagcloud/chart/components/visualization/tag_cloud');
 
 function chartGenerator() {
+  var events = control();
+  var layout = baseLayout();
   var opts = {};
+  var listeners = {};
 
   function generator(selection) {
     selection.each(function (data) {
@@ -19,7 +24,19 @@ function chartGenerator() {
         builder(o, chart);
       });
 
-      d3.select(this).call(chart); // Draw Chart
+      layout.attr({
+        type: opts.layout || 'grid',
+        columns: opts.numOfColumns || 0,
+        size: getSize()
+      });
+
+      events.listeners(eventListeners);
+
+      d3.select(this)
+        .call(events)
+        .call(layout)
+        .selectAll('g.chart')
+        .call(chart); // Draw Chart
     });
   }
 
@@ -27,6 +44,12 @@ function chartGenerator() {
   generator.options = function (v) {
     if (!arguments.length) { return opts; }
     opts = _.isPlainObject(v) && !_.isArray(v) ? v : opts;
+    return generator;
+  };
+
+  generator.listeners = function (v) {
+    if (!arguments.length) { return listeners; }
+    listeners = _.isPlainObject(v) && !_.isArray(v) ? v : listeners;
     return generator;
   };
 
--- public/lib/cloud_directive.js
+++ public/lib/cloud_directive.js
@@ -1,22 +1,18 @@
 var d3 = require('d3');
 var _ = require('lodash');
-var baseLayout = require('plugins/tagcloud/chart/components/layout/generator');
-var control = require('plugins/tagcloud/chart/components/control/events');
 var chartGenerator = require('plugins/tagcloud/chart/index');
 var module = require('ui/modules').get('tagcloud');
 
 module.directive('tagCloud', function () {
   function link (scope, element, attrs) {
-    var layout = baseLayout();
     var chart = chartGenerator();
-    var events = control();
     var svg = d3.select(element[0]);
 
     function onSizeChange() {
       return {
         width: element.parent().parent().width(),
         height: element.parent().parent().height()
-      }
+      };
     }
 
     function getSize() {
@@ -26,25 +22,17 @@ module.directive('tagCloud', function () {
 
     function render(data, opts, eventListeners) {
       opts = opts || {};
+      eventListeners = eventListeners || {};
 
-      events.listeners(eventListeners);
-
-      layout.attr({
-        type: opts.layout || 'grid',
-        columns: opts.numOfColumns || 0,
-        size: getSize()
-      });
-
-      chart.options(opts);
+      chart
+        .options(opts);
+        .listeners(eventListeners);
 
       if (data) {
         svg
           .attr('width', getSize()[0])
           .attr('height', getSize()[1])
           .datum(data)
-          .call(events)
-          .call(layout)
-          .selectAll('g.chart')
           .call(chart);
       }
     };
@@ -54,21 +42,15 @@ module.directive('tagCloud', function () {
     });
 
     scope.$watch('options', function (newVal, oldVal) {
-      if (scope.data) {
-        render(scope.data, newVal, scope.eventListeners);
-      }
+      render(scope.data, newVal, scope.eventListeners);
     });
 
     scope.$watch('eventListeners', function (newVal, oldVal) {
-      if (scope.data) {
-        render(scope.data, scope.options, newVal);
-      }
+      render(scope.data, scope.options, newVal);
     });
 
     scope.$watch(onSizeChange, _.debounce(function () {
-      if (scope.data) {
-        render(scope.data, scope.options, scope.eventListeners);
-      }
+      render(scope.data, scope.options, scope.eventListeners);
     }, 250), true);
 
     element.bind('resize', function () {
