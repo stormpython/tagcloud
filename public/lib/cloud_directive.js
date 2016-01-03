eed12eed0ebd45cf56b128dcce8bdb78daef3657
Shelby Sturgis <shelbyjsturgis@gmail.com>
2016-01-03 11:57:47 -0800

    removing semicolon

--- public/lib/cloud_directive.js
+++ public/lib/cloud_directive.js
@@ -25,7 +25,7 @@ module.directive('tagCloud', function () {
       eventListeners = eventListeners || {};
 
       chart
-        .options(opts);
+        .options(opts)
         .listeners(eventListeners);
 
       if (data) {
