b60ba94f98cbd5965cdada24f0991de85983d882
Shelby Sturgis <shelbyjsturgis@gmail.com>
2016-01-04 00:05:30 -0800

    adding a wait for dom to be ready to directive to deal with [0,0] size on directive dom element, still not working though

--- public/lib/cloud_directive.js
+++ public/lib/cloud_directive.js
@@ -6,52 +6,54 @@ var module = require('ui/modules').get('tagcloud');
 
 module.directive('tagCloud', function () {
   function link (scope, element, attrs) {
-    var vis = visGenerator();
-    var svg = d3.select(element[0]);
+    angular.element(document).ready(function () {
+      var vis = visGenerator();
+      var svg = d3.select(element[0]);
 
-    function onSizeChange() {
-      return {
-        width: element.parent().width(),
-        height: element.parent().height()
-      };
-    }
-
-    function getSize() {
-      var size = onSizeChange();
-      return [size.width, size.height];
-    };
-
-    function render(data, opts, eventListeners) {
-      opts = opts || {};
-      eventListeners = eventListeners || {};
-
-      vis.options(opts)
-        .listeners(eventListeners)
-        .size(getSize());
-
-      if (data) {
-        svg.datum(data).call(vis);
+      function onSizeChange() {
+        return {
+          width: element.parent().width(),
+          height: element.parent().height()
+        };
       }
-    };
 
-    scope.$watch('data', function (newVal, oldVal) {
-      render(newVal, scope.options, scope.eventListeners);
-    });
+      function getSize() {
+        var size = onSizeChange();
+        return [size.width, size.height];
+      };
 
-    scope.$watch('options', function (newVal, oldVal) {
-      render(scope.data, newVal, scope.eventListeners);
-    });
+      function render(data, opts, eventListeners) {
+        opts = opts || {};
+        eventListeners = eventListeners || {};
 
-    scope.$watch('eventListeners', function (newVal, oldVal) {
-      render(scope.data, scope.options, newVal);
-    });
+        vis.options(opts)
+          .listeners(eventListeners)
+          .size(getSize());
 
-    scope.$watch(onSizeChange, _.debounce(function () {
-      render(scope.data, scope.options, scope.eventListeners);
-    }, 250), true);
+        if (data) {
+          svg.datum(data).call(vis);
+        }
+      };
 
-    element.bind('resize', function () {
-      scope.$apply();
+      scope.$watch('data', function (newVal, oldVal) {
+        render(newVal, scope.options, scope.eventListeners);
+      });
+
+      scope.$watch('options', function (newVal, oldVal) {
+        render(scope.data, newVal, scope.eventListeners);
+      });
+
+      scope.$watch('eventListeners', function (newVal, oldVal) {
+        render(scope.data, scope.options, newVal);
+      });
+
+      scope.$watch(onSizeChange, _.debounce(function () {
+        render(scope.data, scope.options, scope.eventListeners);
+      }, 250), true);
+
+      element.bind('resize', function () {
+        scope.$apply();
+      });
     });
   }
 
